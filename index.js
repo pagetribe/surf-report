1347f7539143491327d8a27fa5f3fb95c1b3ec8d
Mark <pagetribemail@gmail.com>
2017-01-24 10:26:28 +1100

    got handlebars working

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 var express = require('express')
 var app = express()
 var path = require('path')
+var exphbs = require('express-handlebars')
 
 var scrape = require("./js/main.js")
 
 app.use(express.static(path.join(__dirname, 'public')))
+app.engine('.hbs', exphbs({defaultLayout: 'layout', extname: '.hbs'}))
+app.set('view engine', '.hbs')
 
 app.get('/', function(req, res) {
-  var costalwatch = scrape.getCoastalWatchReport()
+  // var costalwatch = scrape.getCoastalWatchReport()
   //need a template engine
-  res.sendFile(path.join(__dirname + '/index.html'))
+  res.render('index', { title: 'hello world' });
+  // res.sendFile(path.join(__dirname + '/index.html'))
 })
 
 app.listen(3000, function() {
